900c448ce26c23be410d7319ecbaaa1923a78642
kge <kge@DESKTOP-GJ99IPU>
2022-01-03 23:31:31 +0900

    first commit
    첫번째 커밋

---
+++ board/src/main/java/com/board/domain/BoardVO.java
@@ -0,0 +1,50 @@
+package com.board.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date wdate;
+	private int viewCnt;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getWdate() {
+		return wdate;
+	}
+	public void setWdate(Date wdate) {
+		this.wdate = wdate;
+	}
+	public int getViewCnt() {
+		return viewCnt;
+	}
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}
+	
+}
